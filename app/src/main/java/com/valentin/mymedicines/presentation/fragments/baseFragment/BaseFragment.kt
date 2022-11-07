dd69c28f43fcf9cdcae6b69db41fe9fe6c35ee24
Valen <32tut32@gmail.com>
2022-11-07 15:05:21 +0300

    save args

--- app/src/main/java/com/valentin/mymedicines/presentation/fragments/baseFragment/BaseFragment.kt
+++ app/src/main/java/com/valentin/mymedicines/presentation/fragments/baseFragment/BaseFragment.kt
@@ -6,21 +6,4 @@ import com.valentin.mymedicines.R
 
 abstract class BaseFragment<Binding : ViewBinding>(layoutID: Int) : Fragment(layoutID) {
     protected abstract val binding: Binding
-
-    fun Fragment.replaceFragment(fragment: Fragment, addStack: Boolean = true) {
-        if (addStack) {
-            this.fragmentManager?.beginTransaction()
-                ?.addToBackStack(null)
-                ?.replace(
-                    R.id.fragmentContainerView,
-                    fragment
-                )?.commit()
-        } else{
-            this.fragmentManager?.beginTransaction()
-                ?.replace(
-                    R.id.fragmentContainerView,
-                    fragment
-                )?.commit()
-        }
-    }
 }
